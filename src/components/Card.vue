8f8bd36fbcfd0e76bb0a0581bbaa15db87ec9bf0
Joseph Moussa <joeph465@gmail.com>
2021-05-26 23:02:37 -0400

    add proper formatting and style upgrades

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -4,16 +4,16 @@
     <h4>{{ initialBody.name }}</h4>
     <div v-for="(value, key) in body" :key="key" class="rh-portfolio-card-info">
       <div v-if="left_pill == key && value < 0" class="left-pill red-pill">
-        <span>$ {{ Math.round(100*value)/100 }}</span>
+        <span>{{ formatter.format(value) }}</span>
       </div> 
       <div v-if="left_pill == key && value >= 0" class="left-pill green-pill">
-        <span>$ +{{ Math.round(100*value)/100 }}</span>
+        <span>+{{ formatter.format(value) }}</span>
       </div>
       <div v-if="right_pill == key && value < 0" class="right-pill red-pill">
-        <span>{{ Math.round(100*value)/100 }}%</span>
+        <span>{{ formatter.format(value) }}</span>
       </div>
       <div v-if="right_pill == key && value >= 0" class="right-pill green-pill">
-        <span>+{{ Math.round(100*value)/100 }}%</span>
+        <span>{{ formatter.format(value) }}</span>
       </div>
     </div>
   </div>
@@ -32,7 +32,7 @@ export default {
     visible_fields: {
       type: Array,
       default: function () {
-        return ['percent_change', 'equity_change'];
+        return ['price', 'equity_change'];
       }
     },
     left_pill: {
@@ -41,7 +41,7 @@ export default {
     },
     right_pill: {
       type: String,
-      default: "percent_change"
+      default: "price"
     },
   },
   data: function () {
@@ -51,6 +51,13 @@ export default {
         return obj;
       }, {}),
       title: this.initialTitle,
+      formatter: new Intl.NumberFormat('en-US', {
+          style: 'currency',
+          currency: 'USD',
+          // These options are needed to round to whole numbers if that's what you want.
+          //minimumFractionDigits: 0, // (this suffices for whole numbers, but will print 2500.10 as $2,500.1)
+          //maximumFractionDigits: 0, // (causes 2500.99 to be printed as $2,501)
+        }),
     }
   },
   methods: {
@@ -67,27 +74,27 @@ export default {
   height: 15rem;
   width: 20rem;
   margin: 0;
-  background: linear-gradient(355deg, rgba(2,0,36,1) 0%, rgba(0,0,0,1) 89%, rgba(255,255,255,1) 120%);
+  background: linear-gradient(355deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 89%, rgba(255,255,255,1) 120%);
 }
 
 .portfolio-card:hover {
-  background: linear-gradient(350deg, rgba(2,0,36,1) 0%, rgba(0,0,0,1) 89%, rgba(255,255,255,1) 110%);
+  background: linear-gradient(350deg, rgba(0,0,0,1) 0%, rgba(0,0,0,1) 89%, rgba(255,255,255,1) 110%);
 }
 h3 {
   margin: 1rem;
   color: #42b983;
 }
 .red-pill { 
-  background: #DF2935;
+  background: linear-gradient(355deg, rgba(223, 41, 53, 1) 0%, rgba(223, 41, 53, 1) 89%, rgba(255,255,255,1) 110%);
 }
 .green-pill { 
-  background: #42b983
+  background: linear-gradient(355deg, rgba(66, 185, 131, 1) 0%, rgba(66, 185, 131, 1) 89%, rgba(255,255,255,1) 110%);
 }
 .left-pill {
   z-index: 5;
   float: left;
-  text-decoration: bold;
-  font-size: 1.2rem;
+  font-weight: bold;
+  font-size: 1.5rem;
   text-align: center;
   width: 50%;
   margin: auto;
@@ -98,7 +105,8 @@ h3 {
 .right-pill {
   z-index: 5;
   float: right;
-  font-size: 1.2rem;
+  font-weight: bold;
+  font-size: 1.5rem;
   text-align: center;
   width: 50%;
   margin: auto;
@@ -110,7 +118,8 @@ h3 {
   z-index: 5;
   text-align: center;
   margin: auto;
-  width: 80%;
+  padding: 0 1rem;
+  width: 100%;
   margin-top: 2rem;
 }
 </style>
