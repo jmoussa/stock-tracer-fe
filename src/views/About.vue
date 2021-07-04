5fdf1d052bcfee996e378797079740314222b21b
Joseph Moussa <joeph465@gmail.com>
2021-07-04 00:27:03 -0400

    Update about section

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,13 +1,35 @@
 <template>
   <div class="about">
     <h1>About Stock Tracer</h1>
-    <Button text="Hello World" @buttonClicked="onClick"/>
+    <div class="generic-container">
+      <div class="element"> 
+        <h2>What is Stock Tracer?</h2> 
+        <p>
+          This stock tracer is meant to serve as an extention to your conventional brokerage app.
+          Currently we're basing it off of Robinhood but I later hope to include additional brokerages.
+        </p>
+      </div>
+      <div class="element"> 
+        <h2>What can Stock Tracer do for me?</h2> 
+        <p>
+          Stock Tracer adds additional views and metrics for you to visualize your portfolio.
+        </p>
+        <p>
+          Some of these features include:
+        </p>
+        <ul>
+          <li>Visualizing entrypoints along the historical ticker chart</li>
+          <li>Ability to overlay MACD and other various signal lines over the historical ticker chart</li>
+          <li>Relevant News</li>
+          <li>Yahoo Finance based charts with Robinhood personalized portfolio</li>
+        </ul>
+      </div>
+    </div> 
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
-import Button from '@/components/Button';
 
 export default {
   name: "About",
@@ -15,9 +37,31 @@ export default {
     onClick(){
       console.log("onClick in Parent Component")
     }
-  },
-  components: {
-    Button
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.generic-container {
+  border: 2px solid #42b983;
+  border-radius: 2rem;
+
+  margin: 0 16rem;
+  padding: 2rem;
+  text-align: left;
+  .element {
+    margin: 2rem;
+    padding: 5px 2rem;
+    p {
+      margin: 5px 2rem;
+    }
+    ul {
+      margin: 0 4rem;
+    }
+    
+  }
+}
+.about{
+  margin: 5rem;
+}
+</style>
